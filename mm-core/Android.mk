6e086779863e773ad6906906847401e492964d8a
Praneeth Paladugu <ppaladug@codeaurora.org>
2017-03-20 20:09:00 -0700

    mm-core: Add registry files for msmskunk
    Add registry file and modify makefile to refer code registry
    for msmskunk target.
    
    CRs-Fixed: 2022716
    Change-Id: I3d4cf4f0b1e4f339957c481283eb8c2f91afcfab

--- mm-core/Android.mk
+++ mm-core/Android.mk
@@ -20,6 +20,8 @@ else ifeq ($(filter $(TARGET_BOARD_PLATFORM), $(TRINKET)),$(TARGET_BOARD_PLATFOR
 OMXCORE_CFLAGS += -D_TRINKET_
 else ifeq ($(filter $(TARGET_BOARD_PLATFORM), atoll),$(TARGET_BOARD_PLATFORM))
 OMXCORE_CFLAGS += -D_ATOLL_
+else ifeq ($(filter $(TARGET_BOARD_PLATFORM), sdm845 msmskunk),$(TARGET_BOARD_PLATFORM))
+MM_CORE_TARGET = sdm845
 else
 OMXCORE_CFLAGS += -D_DEFAULT_
 endif
@@ -84,7 +86,9 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter sm8150 $(MSMSTEPPE) $(TRINKET) atoll,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter sdm845 ,$(TARGET_BOARD_PLATFORM)))
+LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table_android.c
+else ifneq (,$(filter sm8150 $(MSMSTEPPE) $(TRINKET) atoll,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/registry_table_android.c
 else
 LOCAL_SRC_FILES         += src/qc_registry_table_android.c
@@ -117,7 +121,9 @@ LOCAL_CFLAGS            := $(OMXCORE_CFLAGS)
 
 LOCAL_SRC_FILES         := src/common/omx_core_cmp.cpp
 LOCAL_SRC_FILES         += src/common/qc_omx_core.c
-ifneq (,$(filter sm8150 $(MSMSTEPPE) $(TRINKET) atoll,$(TARGET_BOARD_PLATFORM)))
+ifneq (,$(filter sdm845 ,$(TARGET_BOARD_PLATFORM)))
+LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table.c
+else ifneq (,$(filter sm8150 $(MSMSTEPPE) $(TRINKET) atoll,$(TARGET_BOARD_PLATFORM)))
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/registry_table.c
 else
 LOCAL_SRC_FILES         += src/$(MM_CORE_TARGET)/qc_registry_table.c
