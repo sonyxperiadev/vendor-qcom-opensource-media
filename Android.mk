7c57b64bbaa26f5308177e5399956734a3300ff3
Alin Jerpelea <alin.jerpelea@sony.com>
2021-03-15 17:34:16 +0100

    Android.mk: add KERNEL version build barrier
    To avoid build errors we have to add build barriers for kernel version
    so that the proper kernel headers are included
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- Android.mk
+++ Android.mk
@@ -1,4 +1,5 @@
 ifneq ($(filter $(QCOM_NEW_MEDIA_PLATFORM), $(TARGET_BOARD_PLATFORM)),)
+ifneq ($(filter 4.14, $(SOMC_KERNEL_VERSION)),)
 
 QCOM_MEDIA_ROOT := $(call my-dir)
 
@@ -15,6 +16,7 @@ include $(QCOM_MEDIA_ROOT)/libc2dcolorconvert/Android.mk
 include $(QCOM_MEDIA_ROOT)/libarbitrarybytes/Android.mk
 ifeq ($(ENABLE_HYP),true)
 include $(QCOM_MEDIA_ROOT)/hypv-intercept/Android.mk
+endif
 endif
 endif
 endif
